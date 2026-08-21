52809fe785cfe554915d1305664b832814cd88d6
Piyush Kumar Sharma <piyushsharmadec009@gmail.com>
2026-08-21 23:37:07 +0530

    Patterns Problems added

---
+++ Patterns/BestPro/OtherHalf.java
@@ -0,0 +1,23 @@
+public class OtherHalf {
+    public static void main(String[] args) {
+        
+        int n = 4;
+
+        for(int i = 1; i<=n; i++) {
+            
+            // spaces
+            
+            for(int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+         
+            // Inner loop for star
+
+            for(int j = 1; j<=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }    
+}
