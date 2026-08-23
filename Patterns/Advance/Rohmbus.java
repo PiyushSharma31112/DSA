2cc2a9194ade7278872eedffd59fc6bfc2261e39
Piyush Kumar Sharma <piyushsharmadec009@gmail.com>
2026-08-23 19:37:26 +0530

    Advance Patterns Added

---
+++ Patterns/Advance/Rohmbus.java
@@ -0,0 +1,23 @@
+public class Rohmbus {
+    public static void main(String[] args) {
+        
+        int n = 5;
+
+        for(int i = 1; i<=n; i++) {
+
+            int spaces = n-i;
+
+            for(int j =1; j<= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+
+            for(int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }    
+}
