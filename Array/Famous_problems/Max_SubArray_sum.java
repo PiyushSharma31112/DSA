1bf06e1a28b54d15d8df7487fb64bc3310554bfb
Piyush Kumar Sharma <piyushsharmadec009@gmail.com>
2026-08-24 23:50:30 +0530

    Added Famous Problem in Array

---
+++ Array/Famous_problems/Max_SubArray_sum.java
@@ -0,0 +1,24 @@
+public class Max_SubArray_sum {
+    public static void main(String[] args) {
+        int arr [] = {
+            -2, 1, -3, 4, -1, 2, 1, -5, 4
+        };
+
+        int current = arr[0];
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            current = Math.max(
+                arr[i],
+                current + arr[i]
+            );
+
+            max = Math.max(max, current);
+        }
+
+        System.out.println(
+            "Maximum Subarray Sum = " + max
+        );
+    }    
+}
