8f1fa842249ceb4b74846f4065b53bfd00c0f470
Piyush Kumar Sharma <piyushsharmadec009@gmail.com>
2026-08-30 23:47:13 +0530

    HashMap Ques

---
+++ HashMap/NonRepeatingChar.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class NonRepeatingChar {
+    public static void main(String[] args) {
+
+        String str = "leetcode";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : str.toCharArray()) {
+            if(map.get(ch) == 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+
+    }    
+}
